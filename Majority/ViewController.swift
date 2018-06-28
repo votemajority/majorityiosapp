fdd4a0f1b3a54ba494ba1d683c868c817e4e6377
nathanielmhld <nathanielmhld@berkeley.edu>
2018-06-27 18:19:51 -0700

    Initial Commit

---
+++ Majority/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Majority
+//
+//  Created by Nathaniel Mahowald on 6/27/18.
+//  Copyright © 2018 Nathaniel Mahowald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
